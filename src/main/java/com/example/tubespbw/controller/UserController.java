78a180cc268a113f20648fb230050cb8a4214606
6182201078 <6182201078@student.unpar.ac.id>
2024-11-26 19:27:27 +0700

    added server

---
+++ src/main/java/com/example/tubespbw/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.tubespbw.controller;
+
+import org.springframework.stereotype.Controller;
+
+// import com.example.tubespbw.model.User;
+
+@Controller
+public class UserController {
+    
+}
