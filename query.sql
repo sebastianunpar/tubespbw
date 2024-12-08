98359522a91d35cd437e8f0a5c93cf540a02c602
6182201078 <6182201078@student.unpar.ac.id>
2024-12-08 16:04:52 +0700

    fixed sql query

--- query.sql
+++ query.sql
@@ -4,30 +4,43 @@ CREATE TABLE users (
     email VARCHAR(70),
     password VARCHAR(50),
     role VARCHAR(10) CHECK (role IN ('user', 'admin'))
-
-
-)
+);
 
 CREATE TABLE film (
     filmId SERIAL PRIMARY KEY,
     title VARCHAR(30),
+    synopsis VARCHAR(500),
+    image BYTEA,
+    stock INT CHECK (stock >= 0),
+    sold INT,
     price NUMERIC
-
-)
+);
 
 CREATE TABLE actor (
     actorId SERIAL PRIMARY KEY,
     name VARCHAR(50)
-)
+);
 
 CREATE TABLE genre (
     genreId SERIAL PRIMARY KEY,
     name VARCHAR(20)
-)
+);
 
+CREATE TABLE filmActor (
+    filmId INT NOT NULL REFERENCES film(filmId),
+    actorId INT NOT NULL REFERENCES actor(actorId),
+    PRIMARY KEY (filmId, actorId)
+);
+
+CREATE TABLE filmGenre (
+    filmId INT NOT NULL REFERENCES film(filmId),
+    genreId INT NOT NULL REFERENCES genre(genreId),
+    PRIMARY KEY (filmId, genreId)
+);
 
 CREATE TABLE rental (
     rentalId SERIAL PRIMARY KEY,
-    dateRented DATE,
-    
-)
+    rentalDate DATE NOT NULL,
+    dueDate DATE NOT NULL,
+    returnDate DATE
+);
