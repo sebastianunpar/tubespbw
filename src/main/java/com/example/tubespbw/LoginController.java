e2c31d4f1521884442f706c2ce256d05d5ebe9f9
Fauzan Rhamzy <fauzanrhamzy04@gmail.com>
2024-12-12 11:33:09 +0700

    Add Movie revision and initial login setup

--- src/main/java/com/example/tubespbw/LoginController.java
+++ src/main/java/com/example/tubespbw/LoginController.java
@@ -6,8 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.tubespbw.user.User;
+
+import main.java.com.example.tubespbw.user.UserService;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/login")
     public String showLogin() {
         return "login";
@@ -15,6 +23,14 @@ public class LoginController {
 
     @PostMapping("login")
     public String login(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+        User user = userService.login(email, password);
+        if (User != null ) {
+            if (user.getRole() == "admin") {
+                return "redirect:/admin";
+            }
+            else if (user.getRole() == "user")
+            return "home";
+        }
         return "redirect:";
     }
 
