b72ae39bb16106982868733075c0ecfe39b9b8f7
6182201078 <6182201078@student.unpar.ac.id>
2024-12-20 02:57:34 +0700

    uncommented admin page

--- src/main/java/com/example/tubespbw/admin/AdminController.java
+++ src/main/java/com/example/tubespbw/admin/AdminController.java
@@ -1,50 +1,50 @@
-// package com.example.tubespbw.admin;
+package com.example.tubespbw.admin;
 
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
-// @Controller
-// @RequestMapping("/admin")
-// public class AdminController {
-//     @GetMapping({"", "/"})
-//     public String showHome() {
-//         return "admin/home";
-//     }
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping({"", "/"})
+    public String showHome() {
+        return "admin/home";
+    }
 
-//     @GetMapping("/monthly-report")
-//     public String showMonthlyReport() {
-//         return "admin/monthlyReport";
-//     }
+    @GetMapping("/monthly-report")
+    public String showMonthlyReport() {
+        return "admin/monthlyReport";
+    }
 
-//     @GetMapping("/income")
-//     public String showIncome() {
-//         return "admin/incomeGraph";
-//     }
+    @GetMapping("/income")
+    public String showIncome() {
+        return "admin/incomeGraph";
+    }
 
-//     @GetMapping("/film-graph")
-//     public String showFilmGraph() {
-//         return "admin/filmGraph";
-//     }
+    @GetMapping("/film-graph")
+    public String showFilmGraph() {
+        return "admin/filmGraph";
+    }
 
-//     @GetMapping("/add-movie")
-//     public String showAddMovie() {
-//         return "admin/addMovie";
-//     }
+    @GetMapping("/add-movie")
+    public String showAddMovie() {
+        return "admin/addMovie";
+    }
 
-//     @GetMapping("/manage-actors")
-//     public String showManageActors() {
-//         return "admin/manageActors";
-//     }
+    @GetMapping("/manage-actors")
+    public String showManageActors() {
+        return "admin/manageActors";
+    }
 
-//     @GetMapping("/manage-genres")
-//     public String showManageGenres() {
-//         return "admin/manageGenres";
-//     }
+    @GetMapping("/manage-genres")
+    public String showManageGenres() {
+        return "admin/manageGenres";
+    }
 
-//     @GetMapping("/report")
-//     public String showReport() {
-//         return "admin/report";
-//     }
-// }
+    @GetMapping("/report")
+    public String showReport() {
+        return "admin/report";
+    }
+}
