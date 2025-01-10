e16488f79e0ba7e5c14964234c304ffd6a1ae34e
6182201078 <6182201078@student.unpar.ac.id>
2025-01-10 14:22:10 +0700

    added rent film

--- src/main/java/com/example/tubespbw/film/FilmRepository.java
+++ src/main/java/com/example/tubespbw/film/FilmRepository.java
@@ -38,4 +38,6 @@ public interface FilmRepository {
     int getFilmCount();
     int getFilmCountByName(String movieName);
     int getFilmCountByActorAndGenre(List<String> actorNames, List<String> genreNames, String movieName);
+    boolean removeFilmStock(int filmId);
+    boolean addFilmStock(int filmId);
 }
--- src/main/java/com/example/tubespbw/rental/RentalService.java
+++ src/main/java/com/example/tubespbw/rental/RentalService.java
@@ -6,10 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.tubespbw.film.FilmService;
+
 @Service
 public class RentalService {
     @Autowired
     private RentalRepository rentalRepo;
+
+    @Autowired
+    FilmService filmService;
 
     public List<Rental> getUserRentals(int userId) {
         return rentalRepo.getUserRentals(userId);
@@ -19,11 +24,10 @@ public class RentalService {
         return rentalRepo.getUserRentalHistory(userId);
     }
 
-    public boolean insertRental(int filmId, int userId, String metodePembayaran, String noPembayaran) {
+    public boolean insertRental(int filmId, int userId, String metodePembayaran) {
         LocalDate rentalDate = LocalDate.now();
-        LocalDate dueDate = rentalDate.plusDays(7);
-
-        return rentalRepo.insertRental(rentalDate, dueDate, filmId, userId, metodePembayaran, noPembayaran);
+        filmService.removeFilmStock(filmId);
+        return rentalRepo.insertRental(rentalDate, filmId, userId, metodePembayaran);
     }
 
     public List<Integer> getRentalsPerMonth() {
@@ -33,6 +37,6 @@ public class RentalService {
 
     public List<Double> getIncomePerMonth() {
         int year = LocalDate.now().getYear();
-        return rentalRepo.getIncomePerMonth(2024);
+        return rentalRepo.getIncomePerMonth(year);
     }
 }
